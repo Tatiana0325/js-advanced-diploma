3c973748fc5b20af556930ff0cdc1a3bcf92fa8a
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-10-06 13:13:15 +0300

    cganging test

--- src/__tests__/Character_test.js
+++ src/__tests__/Character_test.js
@@ -1,4 +1,4 @@
-import Character from "../Character";
+import Character from "../js/Character";
 
 test("проверка создания класса через Character", () => {
   expect(() => new Character()).toThrow();
