3073bdaa291a538bfa18d7487db439f81af73814
Tatiana Krasnova <krasnova-tatyana92@mail.ru>
2020-10-06 13:33:39 +0300

    third commit

--- src/js/Team.js
+++ src/js/Team.js
@@ -1,20 +1,3 @@
-import Bowman from "./characters/Bowman";
-import Swordsman from "./characters/Swordsman";
-import Magician from "./characters/Magician";
-import Daemon from "./characters/Daemon";
-import Vampire from "./characters/Vampire";
-import Undead from "./characters/Undead";
-
 export default class Team {
-  static getStartUserTeam() {
-    return [new Bowman(1), new Swordsman(1)];
-  }
 
-  static getUserTeam() {
-    return [Bowman, Swordsman, Magician];
-  }
-
-  static getEnemyTeam() {
-    return [Daemon, Undead, Vampire];
-  }
 }
